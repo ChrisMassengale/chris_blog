0b4c965c192c5dce915ba0d062e46d0af94ccbb3
Chris Massengale <chris.massengale@verizon.net>
2017-02-01 17:32:37 -0500

    Add Devise Authentication. Still work to do

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def authorized_editor?
+    if current_user && current_user.email == 'chris.massengale@verizon.net'
+      return true
+    else
+      return false
+    end 
+  end 
 end
